d9cfeb2d0bf02f5838ecd422534bbfdaeee03dfc
dolphinium <yunusemrekorkmaz@eskisehir.edu.tr>
2021-06-06 22:57:19 +0300

    day 9 assignment added.

--- day7-8/hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/EmployeeCandidateDao.java
+++
@@ -1,8 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import kodlamaio.hrms.entities.concretes.EmployeeCandidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeCandidateDao extends JpaRepository<EmployeeCandidate,Integer> {
-}
--- day7-8/hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/SystemStaffDao.java
+++
@@ -1,7 +0,0 @@
-package kodlamaio.hrms.dataAccess.abstracts;
-
-import kodlamaio.hrms.entities.concretes.SystemStaff;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SystemStaffDao extends JpaRepository<SystemStaff,Integer> {
-}
